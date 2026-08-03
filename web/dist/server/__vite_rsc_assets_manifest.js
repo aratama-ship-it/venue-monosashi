312db1ec2ac1ee01c0141b282ce14296d29745fc
Arata <arata@AratanoMac-mini.lan>
2026-08-03 20:26:47 +0900

    Package validated venue site build for hosting

---
+++ web/dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,47 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-D03hlG5d.js\")",
+  "clientReferenceDeps": {
+    "54e7cc413194": {
+      "js": [
+        "/assets/venue-search-CPfZA03m.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-DjPHiq1u.js",
+        "/assets/index-D03hlG5d.js"
+      ],
+      "css": []
+    },
+    "593f344dc510": {
+      "js": [
+        "/assets/index-D03hlG5d.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-DjPHiq1u.js"
+      ],
+      "css": []
+    },
+    "15c18cfaeeff": {
+      "js": [
+        "/assets/layout-segment-context-B1WlJcf_.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/index-D03hlG5d.js",
+        "/assets/framework-DjPHiq1u.js"
+      ],
+      "css": []
+    },
+    "8c0f216c4604": {
+      "js": [
+        "/assets/index-D03hlG5d.js",
+        "/assets/rolldown-runtime-S-ySWqyJ.js",
+        "/assets/framework-DjPHiq1u.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-Cc8TkZyn.css"
+      ]
+    }
+  }
+}
