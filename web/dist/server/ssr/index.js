679d668a945ca9c56b0ac5619a75958cb8403ae6
Arata <arata@AratanoMac-mini.local>
2026-08-05 03:52:19 +0900

    Add eighth regional gymnasium wave

--- web/dist/server/ssr/index.js
+++ web/dist/server/ssr/index.js
@@ -11634,7 +11634,7 @@ var client_references_default = {
 		} };
 	},
 	"54e7cc413194": async () => {
-		const m = await import("./assets/venue-search-FGWAlC6R.js");
+		const m = await import("./assets/venue-search-BEpCOO4p.js");
 		return { get "VenueSearch"() {
 			return m["VenueSearch"];
 		} };
