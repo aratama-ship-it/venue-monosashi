36349f1de29a05926f6f417d5f4835c67a76ec50
Arata <arata@AratanoMac-mini.lan>
2026-08-05 03:07:09 +0900

    Add third regional gymnasium wave

--- web/dist/server/__vite_rsc_assets_manifest.js
+++ web/dist/server/__vite_rsc_assets_manifest.js
@@ -1,18 +1,18 @@
 export default {
-  "bootstrapScriptContent": "import(\"/assets/index-sBVPtJsX.js\")",
+  "bootstrapScriptContent": "import(\"/assets/index-BdaEY5Z6.js\")",
   "clientReferenceDeps": {
     "54e7cc413194": {
       "js": [
-        "/assets/venue-search-XpmK7k-T.js",
+        "/assets/venue-search-CqR_D-Vh.js",
         "/assets/rolldown-runtime-S-ySWqyJ.js",
         "/assets/framework-DjPHiq1u.js",
-        "/assets/index-sBVPtJsX.js"
+        "/assets/index-BdaEY5Z6.js"
       ],
       "css": []
     },
     "593f344dc510": {
       "js": [
-        "/assets/index-sBVPtJsX.js",
+        "/assets/index-BdaEY5Z6.js",
         "/assets/rolldown-runtime-S-ySWqyJ.js",
         "/assets/framework-DjPHiq1u.js"
       ],
@@ -20,16 +20,16 @@ export default {
     },
     "15c18cfaeeff": {
       "js": [
-        "/assets/layout-segment-context-DpOePhB4.js",
+        "/assets/layout-segment-context-CyX1XB6L.js",
         "/assets/rolldown-runtime-S-ySWqyJ.js",
-        "/assets/index-sBVPtJsX.js",
+        "/assets/index-BdaEY5Z6.js",
         "/assets/framework-DjPHiq1u.js"
       ],
       "css": []
     },
     "8c0f216c4604": {
       "js": [
-        "/assets/index-sBVPtJsX.js",
+        "/assets/index-BdaEY5Z6.js",
         "/assets/rolldown-runtime-S-ySWqyJ.js",
         "/assets/framework-DjPHiq1u.js"
       ],
--- web/dist/server/ssr/index.js
+++ web/dist/server/ssr/index.js
@@ -11634,7 +11634,7 @@ var client_references_default = {
 		} };
 	},
 	"54e7cc413194": async () => {
-		const m = await import("./assets/venue-search-K_MH1ZW_.js");
+		const m = await import("./assets/venue-search-k_2T_zNM.js");
 		return { get "VenueSearch"() {
 			return m["VenueSearch"];
 		} };
